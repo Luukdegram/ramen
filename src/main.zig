11568857e27b9fee4d2bb85ec1a1b990747715b7
Luuk de Gram <Luukdegram@users.noreply.github.com>
2021-04-29 13:20:55 +0200

    Better cli frontend

--- src/main.zig
+++ src/main.zig
@@ -1,16 +1,43 @@
 const std = @import("std");
 const TorrentFile = @import("torrent_file.zig").TorrentFile;
 
-pub fn main() anyerror!void {
+pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
 
-    var path = "bin/slackware-14.2-install-dvd.torrent";
+    // arena for our args
+    var arena = std.heap.ArenaAllocator.init(&gpa.allocator);
+    defer arena.deinit();
+
+    const std_in = std.io.getStdIn().writer();
+
+    var file_path: ?[]const u8 = null;
+    var dir_path: ?[]const u8 = null;
+
+    var args_iterator = std.process.ArgIterator.init();
+    var index: usize = 0;
+    while (args_iterator.next(&arena.allocator)) |maybe_arg| : (index += 1) {
+        const arg = maybe_arg catch continue;
+        if (index == 1) {
+            file_path = arg;
+        }
+
+        if (std.mem.eql(u8, arg, "-d")) {
+            if (args_iterator.next(&arena.allocator)) |maybe_dir_path| {
+                dir_path = maybe_dir_path catch continue;
+            }
+        }
+    }
+
+    const path = file_path orelse {
+        try std_in.writeAll("Missing file argument\n");
+        return;
+    };
+
     var torrent = try TorrentFile.open(&gpa.allocator, path);
     defer torrent.deinit(&gpa.allocator);
 
-    var save_path = "bin";
-    torrent.download(&gpa.allocator, save_path) catch |err| {
+    torrent.download(&gpa.allocator, dir_path) catch |err| {
         std.debug.print("Could not download torrent: {s}\n", .{err});
         return err;
     };
